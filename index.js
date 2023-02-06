a1e83b21d7d3a50587230dc85727de8480107e0d
rumon5h <mdrumonkhan48519@gmail.com>
2023-02-06 22:04:50 +0600

    fixed some issue

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
+const dotenv = require('dotenv').config();
+const colors = require('colors');
 
 app.use(express.json());
 app.use(cors());
@@ -16,5 +19,11 @@ const booksRoute = require('./routes/books.route');
 
 app.use('/api/v1/books', booksRoute);
 
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log('Successfully connected to the database'.blue.bold);
+})
 
-module.exports = app;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port'.red.bold, PORT);
+});
